f2adecee7c766b8a45395b2898570bec286ee14b
Fabien BERNARD <fabien0102@hotmail.com>
2020-01-13 16:40:30 +0100

    Replace @operational/scripts with tsdx

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "rules": {
+    "array-callback-return": "off"
+  }
+}
